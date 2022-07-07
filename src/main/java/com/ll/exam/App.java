db54d73a166244d14929211acb754127a52f8361
SangHun <shzero211@naver.com>
2022-07-07 14:32:38 +0900

    명령 프롬프트 생성

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -1,7 +1,20 @@
 package com.ll.exam;
 
+import java.util.Scanner;
+
 public class App {
     public void run(){
         System.out.println("-----명언 제조기-----");
+        Scanner sc=new Scanner(System.in);
+        outer:
+        while(true){
+            System.out.println("명령)");
+            String cmd=sc.nextLine().trim();
+            switch (cmd){
+                case "종료":
+                    break outer;
+            }
+        }
+        sc.close();
     }
 }
