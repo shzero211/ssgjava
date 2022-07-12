74790a778da4868a64527cefc8700d469cfb4937
SangHun <shzero211@naver.com>
2022-07-12 15:32:36 +0900

    강사님 WiseSayingRepository 추가

--- src/main/java/com/ll/exam/pro/WiseSayingController.java
+++ src/main/java/com/ll/exam/pro/WiseSayingController.java
@@ -1,18 +1,13 @@
 package com.ll.exam.pro;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class WiseSayingController {
     private Scanner sc;
-    private List<WiseSaying> wiseSayings;
-    private int wiseSayingLastId;
+    private WiseSayingRepository wiseSayingRepository;
     WiseSayingController(Scanner sc){
       this.sc=sc;
-      wiseSayings=new ArrayList<>();
-      wiseSayingLastId=0;
-
+        wiseSayingRepository=new WiseSayingRepository();
     }
     void modify(Rq rq) {
         int paramId=rq.getIntParam("id",0);
@@ -20,7 +15,7 @@ public class WiseSayingController {
             System.out.println("id를 입력해주세요.");
             return;
         }
-        WiseSaying foundWiseSaying=findById(paramId);
+        WiseSaying foundWiseSaying=wiseSayingRepository.findById(paramId);
         if(foundWiseSaying==null){
             System.out.printf("%d번 명언은 존재하지 않습니다.\n",paramId);
             return;
@@ -41,18 +36,18 @@ public class WiseSayingController {
         String content = sc.nextLine().trim();
         System.out.printf("작가 : ");
         String author = sc.nextLine().trim();
-        int id = ++wiseSayingLastId; // 명언 글 번호 증가
+        int id = ++wiseSayingRepository.wiseSayingLastId; // 명언 글 번호 증가
 
         WiseSaying wiseSaying = new WiseSaying(id, content, author);
-        wiseSayings.add(wiseSaying);
+       wiseSayingRepository.wiseSayings.add(wiseSaying);
 
         System.out.printf("%d번 명언이 등록되었습니다.\n", id);
     }
     void list(Rq rq) {
         System.out.println("번호 / 작가 / 명언");
         System.out.println("-------------------");
-        for (int i = wiseSayings.size() - 1; i >= 0; i--) {
-            WiseSaying wiseSaying_ = wiseSayings.get(i);
+        for (int i = wiseSayingRepository.wiseSayings.size() - 1; i >= 0; i--) {
+            WiseSaying wiseSaying_ = wiseSayingRepository.wiseSayings.get(i);
             System.out.printf("%d / %s / %s\n", wiseSaying_.id, wiseSaying_.content, wiseSaying_.author);
         }
     }
@@ -63,7 +58,7 @@ public class WiseSayingController {
             return;
         }
         WiseSaying foundwiseSaying=null;
-        for(WiseSaying wiseSaying: wiseSayings){
+        for(WiseSaying wiseSaying: wiseSayingRepository.wiseSayings){
             if(wiseSaying.id==paramId){
                 foundwiseSaying=wiseSaying;
             }
@@ -72,11 +67,11 @@ public class WiseSayingController {
             System.out.printf("%d번 명언은 존재하지 않습니다..\n",paramId);
             return;
         }
-        wiseSayings.remove(foundwiseSaying);
+        wiseSayingRepository.wiseSayings.remove(foundwiseSaying);
         System.out.printf("%d 번 명언이 삭제되었습니다.\n",paramId);
     }
     private WiseSaying findById(int paramId){
-        for(WiseSaying wiseSaying:wiseSayings){
+        for(WiseSaying wiseSaying: wiseSayingRepository.wiseSayings){
             if(wiseSaying.id==paramId){
                 return wiseSaying;
             }
--- src/main/java/com/ll/exam/pro/WiseSayingRepository.java
+++ src/main/java/com/ll/exam/pro/WiseSayingRepository.java
@@ -1,5 +1,22 @@
 package com.ll.exam.pro;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class WiseSayingRepository {
+     List<WiseSaying> wiseSayings;
+    int wiseSayingLastId;
+    WiseSayingRepository(){
+        wiseSayings=new ArrayList<>();
+        wiseSayingLastId=0;
+    }
+    public WiseSaying findById(int paramId){
+        for(WiseSaying wiseSaying:wiseSayings){
+            if(wiseSaying.id==paramId){
+                return wiseSaying;
+            }
+        }
+        return null;
+    }
 
 }
